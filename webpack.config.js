86ec331f4dcdaa45c07177a8b44009cab6a9b7f7
Tim Nelke <tim.nelke@outlook.com>
2021-06-17 01:06:35 +0200

    Finalized `webpack.config.js` - Add babel config - Remove `rollup` dependencies - Remove `rollup.config.js`

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackShellPlugin = require("webpack-shell-plugin-next");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -7,25 +8,55 @@ const stylesHandler = MiniCssExtractPlugin.loader;
 
 const config = {
   entry: "./src/index.ts",
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+    "next/router": "next/router",
+    "next/link": "next/link",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: {
+      type: "umd2",
+      name: "miuri",
+    },
+    globalObject: "this",
+    clean: true,
   },
   plugins: [
     new MiniCssExtractPlugin(),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new WebpackShellPlugin({
+      onBuildExit: {
+        scripts: [
+          "echo 'Copy ./dist/ to playground...'",
+          "rm -r ./playground/components/miuri",
+          "cp -r ./dist/ ./playground/components/miuri",
+        ],
+        blocking: true,
+        parallel: false,
+      },
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
+        use: [{ loader: "babel-loader" }, { loader: "ts-loader" }],
         exclude: ["/node_modules/"],
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
+        use: [
+          stylesHandler,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
