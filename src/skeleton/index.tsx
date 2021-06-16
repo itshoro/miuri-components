570764909cfd0d9febed359fcaef75931bdd8d63
Tim Nelke <tim.nelke@outlook.com>
2021-06-17 01:11:46 +0200

    Implement `Skeleton` - Add `*.module.css` support

--- src/skeleton/index.tsx
+++ src/skeleton/index.tsx
@@ -1,11 +1,12 @@
 import React, { ReactNode } from "react";
+import style from "./index.module.css";
 
 const Skeleton = ({ children, className, show = true }: SkeletonArgs) => {
   if (children && !show) return <>{children}</>;
 
   return (
     <span
-      className={["skeleton rounded inline-block select-none", className]
+      className={[style.skeleton, "rounded inline-block select-none", className]
         .filter((x) => x)
         .join(" ")}
     >
